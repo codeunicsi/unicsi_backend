58487c3bc329f4edd7ce1ac33581c9242143705b
Mohdniyam <niyamul.haque98@gmail.com>
2026-01-30 12:11:08 +0530

    feat: Configure PostgreSQL SSL connection using a CA certificate from `ca.pem`.

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import fs from "fs";
+
+
 
 dotenv.config();
+const sslValue = fs.readFileSync("./ca.pem");
 
-console.log(process.env.DB_NAME)
+// console.log("sslValue", sslValue)
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -13,12 +17,13 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: "postgres",
     port: process.env.DB_PORT,
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false, // Render requires this to avoid self-signed certificate errors
-    //   },
-    // },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Render requires this to avoid self-signed certificate errors
+        ca: sslValue,
+      },
+    },
     logging: false,
   }
 );
@@ -34,3 +39,42 @@ export const connectDB = async () => {
 };
 
 export default sequelize;
+
+
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// console.log(process.env.DB_NAME)
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     port: process.env.DB_PORT,
+//     // dialectOptions: {
+//     //   ssl: {
+//     //     require: true,
+//     //     rejectUnauthorized: false, // Render requires this to avoid self-signed certificate errors
+//     //   },
+//     // },
+//     logging: false,
+//   }
+// );
+
+// export const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("✅ PostgreSQL connected successfully!");
+//   } catch (error) {
+//     console.error("❌ Database connection failed:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default sequelize;
+
