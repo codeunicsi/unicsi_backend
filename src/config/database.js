d223377cbe6ac26ad1fa00639623c9804080001e
Mohdniyam <niyamul.haque98@gmail.com>
2026-02-20 15:33:14 +0530

    feat: Introduce express-session for Shopify OAuth state management, refactor Shopify connection routes, and simplify database SSL configuration.

--- src/config/database.js
+++ src/config/database.js
@@ -1,52 +1,13 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-import fs from "fs";
-
-
-
-dotenv.config();
-const sslValue = fs.readFileSync("./ca.pem");
-
-// console.log("sslValue", sslValue)
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Render requires this to avoid self-signed certificate errors
-        ca: sslValue,
-      },
-    },
-    logging: false,
-  }
-);
-
-export const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ PostgreSQL connected successfully!");
-  } catch (error) {
-    console.error("❌ Database connection failed:", error.message);
-    process.exit(1);
-  }
-};
-
-export default sequelize;
-
-
 // import { Sequelize } from "sequelize";
 // import dotenv from "dotenv";
+// import fs from "fs";
+
+
 
 // dotenv.config();
+// const sslValue = fs.readFileSync("./ca.pem");
 
-// console.log(process.env.DB_NAME)
+// // console.log("sslValue", sslValue)
 
 // const sequelize = new Sequelize(
 //   process.env.DB_NAME,
@@ -56,12 +17,13 @@ export default sequelize;
 //     host: process.env.DB_HOST,
 //     dialect: "postgres",
 //     port: process.env.DB_PORT,
-//     // dialectOptions: {
-//     //   ssl: {
-//     //     require: true,
-//     //     rejectUnauthorized: false, // Render requires this to avoid self-signed certificate errors
-//     //   },
-//     // },
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false, // Render requires this to avoid self-signed certificate errors
+//         ca: sslValue,
+//       },
+//     },
 //     logging: false,
 //   }
 // );
@@ -77,4 +39,42 @@ export default sequelize;
 // };
 
 // export default sequelize;
+
+
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.DB_NAME)
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT,
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false, // Render requires this to avoid self-signed certificate errors
+    //   },
+    // },
+    logging: false,
+  }
+);
+
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ PostgreSQL connected successfully!");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default sequelize;
 
